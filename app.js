3a386a140bd7609a419ed69b0e391288fd80f27c
Umidjon-Fullstack <umidjonyusufov14011999@gmail.com>
2022-05-25 22:19:44 +0500

    finished

---
+++ app.js
@@ -0,0 +1,61 @@
+const pokeContainer = document.querySelector(".poke-container");
+const count = 150;
+const colors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
+
+const url = ` https://pokeapi.co/api/v2/pokemon/${count}`;
+
+async function getFetchData() {
+  for (let i = 1; i <= count; i++) {
+    await getPokemon(i);
+  }
+}
+
+async function getPokemon(id) {
+  const url = ` https://pokeapi.co/api/v2/pokemon/${id}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  createPokemon(data);
+}
+
+function createPokemon(item) {
+  const id = item.id.toString().padStart(3, "0");
+  const name = item.name.toUpperCase();
+  const type = item.types[0].type.name;
+  const color = colors[`${type}`];
+
+  const pokemon = document.createElement("div");
+  pokemon.classList.add("pokemon");
+  pokemon.style.backgroundColor = color;
+  pokemon.innerHTML = `
+    <div class="img-container">
+      <img
+        src=${item.sprites.back_default}
+        alt=""
+      />
+    </div>
+    <div class="info">
+      <span class="number">#${id}</span>
+      <h3 class="name">${name}</h3>
+      <small class="type">Type: <span>${type}</span> </small>
+    </div>
+  `;
+  pokeContainer.appendChild(pokemon);
+}
+
+getFetchData();
